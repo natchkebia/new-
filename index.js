852136632c85d1352e3320c324615ab059ca6bea
tamar <tamarnatchkebia2@gmail.com>
2023-03-25 13:59:46 +0400

    new

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1. იუთუბის კომენტარი
+
+let userLoggedIn = true
+let videosCommentable = true
+const userCanComment = userLoggedIn && videosCommentable
+console.log(userCanComment)
+
+// 2. სოც ქსელი 
+
+let userIsLoggedIn = true
+let userIsBlocked = false
+const UserCanPost = userIsLoggedIn && !userIsBlocked
+console.log(UserCanPost)
+
+// 3. ონლაინ მაღაზია
+
+let balance = 100
+let userCartTotal = 150
+const paymentComplete = balance >= userCartTotal
+console.log(paymentComplete)
+
+// 4. კომენტაის წაშლა
+
+let userIsAdmin = false
+let userIsModerator = false
+let userIsCommentAuthor = true
+const userCanDelete = userIsAdmin || userIsModerator || userIsCommentAuthor
+console.log(userCanDelete)
+
+// 5. ატომური ბომბი
+
+let presidentKey = false
+let primeMinisterKey = false
+let generalOfArmiesKey = false
+let masterKey = true
+const bombWillLaunch = (presidentKey && presidentKey && generalOfArmiesKey) || masterKey
+console.log(bombWillLaunch)
